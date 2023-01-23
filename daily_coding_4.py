201d2d8a32da44a306a40b85e4bcf977a65c0b11
shahsalman <60047165+shahsalman@users.noreply.github.com>
2023-01-23 12:57:12 -0500

    changed to sample text instead of continue

--- daily_coding_4.py
+++ daily_coding_4.py
@@ -30,7 +30,7 @@ def func(arr):
             return True
         else:
             arr.insert(i, tmp)
-            continuedon # correct spelling in fork repo
+            sample text # correct spelling in fork repo
     return False
 
 print(func([1,2,500,270,100,8900]))
